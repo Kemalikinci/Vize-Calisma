a130ce2b8511aea784e560fc3fa3dbc78eccee3c
kemalikinci <kemalikinci1905@gmail.com>
2021-12-02 10:16:28 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp1/Basamaklara-Ayirma.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t\t\tLütfen Basamaklarına ayırmak istediğiniz sayıyı giriniz:");
+            int s1 = Convert.ToInt32(Console.ReadLine()); Console.Clear(); Console.WriteLine("Sayı:"+s1+"\n");
+
+            int bin = s1 / 1000; Console.WriteLine("1000 *:" + bin); s1 = s1 - (bin * 1000);
+            int yuz = s1 / 100 ; Console.WriteLine("100 * :" + yuz); s1 = s1 - (yuz * 100 );
+            int on  = s1 / 10  ; Console.WriteLine("10 *  :" + on ); s1 = s1 - (on  * 10  );
+            int bir = s1       ; Console.WriteLine("1 *   :" + bir);
+            Console.ReadLine();
+        }
+    }
+}
